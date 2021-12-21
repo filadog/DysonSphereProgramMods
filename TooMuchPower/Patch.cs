179ce1e9502b777a181b9a2c7eea83964f4b5d34
filadog <filimonov1995@gmail.com>
2021-12-21 12:12:26 +0300

    Add project files.

---
+++ TooMuchPower/Patch.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+
+namespace TooMuchPower
+{
+    public class Patch
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(UIDysonPanel), "_OnUpdate")]
+        static void OnUpdatePatched(UIDysonPanel __instance)
+        {
+            __instance.genEnergyText.text = GeneratePowerValue(__instance.viewDysonSphere.energyGenCurrentTick * 60, TargetValue.MW);
+        }
+
+        private static string GeneratePowerValue(long totalEnergyGen, TargetValue targetValue)
+        {
+            // Remove trailing numbers according to the target power display value
+            string value = totalEnergyGen.ToString();
+            int length = value.Length;
+            string sign = "W";
+            if (totalEnergyGen != 0)
+            {
+                if (targetValue >= TargetValue.kW && length > 3)
+                {
+                    sign = " kW";
+                    length -= 3;
+                }
+                if (targetValue >= TargetValue.MW && length > 3)
+                {
+                    sign = " MW";
+                    length -= 3;
+                }
+                if (targetValue >= TargetValue.GW && length > 3)
+                {
+                    sign = " GW";
+                    length -= 3;
+                }
+                if (targetValue >= TargetValue.TW && length > 3)
+                {
+                    sign = " TW";
+                    length -= 3;
+                }
+                if (targetValue >= TargetValue.PW && length > 3)
+                {
+                    sign = " PW";
+                    length -= 3;
+                }
+            }
+            value = value.Remove(length);
+            value += sign;
+            return value;
+        }
+
+        enum TargetValue
+        {
+            kW = 3,
+            MW = 6,
+            GW = 9,
+            TW = 12,
+            PW = 15
+        }
+    }
+}
---
+++ TooMuchPower/TooMuchPower.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace TooMuchPower
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class TooMuchPower : BaseUnityPlugin
+    {
+        public const string pluginGuid = "com.filadog.toomuchpower";
+        public const string pluginName = "TooMuchPower";
+        public const string pluginVersion = "1.0.0";
+        
+        public void Awake()
+        {
+            var harmony = new Harmony(pluginGuid);
+            harmony.PatchAll(typeof(Patch));
+        }
+    }
+}
